9cec19620d29551ff6ef841109a638ebed45ecf4
Ariele Amorim <ariele-plra@hotmail.com>
2023-06-02 12:08:54 +0200

    ok

--- app.py
+++ app.py
@@ -16,12 +16,12 @@ date_time = st.date_input("Date")
 time = st.time_input("Time")
 
 # Pickup longitude and latitude
-pickup_longitude = st.number_input("Pickup Longitude")
-pickup_latitude = st.number_input("Pickup Latitude")
+pickup_longitude = str(st.number_input("Pickup Longitude"))
+pickup_latitude = str(st.number_input("Pickup Latitude"))
 
 # Dropoff longitude and latitude
-dropoff_longitude = st.number_input("Dropoff Longitude")
-dropoff_latitude = st.number_input("Dropoff Latitude")
+dropoff_longitude = str(st.number_input("Dropoff Longitude"))
+dropoff_latitude = str(st.number_input("Dropoff Latitude"))
 
 # Passenger count
 passenger_count = st.number_input("Passenger Count", min_value=1, step=1)
@@ -57,21 +57,3 @@ prediction = get_prediction(params)
 ## Finally, we can display the prediction to the user
 '''
 st.success(f'The predicted taxi fare is {prediction} USD')
-
-# Create a base map centered on New York City
-map = folium.Map(location=[40.7128, -74.0060], zoom_start=12)
-
-# Add a marker for pickup location
-pickup_marker = folium.Marker(location=[pickup_latitude, pickup_longitude],
-                              popup='Pickup Location',
-                              icon=folium.Icon(color='blue'))
-map.add_child(pickup_marker)
-
-# Add a marker for dropoff location
-dropoff_marker = folium.Marker(location=[dropoff_latitude, dropoff_longitude],
-                               popup='Dropoff Location',
-                               icon=folium.Icon(color='red'))
-map.add_child(dropoff_marker)
-
-# Display the map using Streamlit
-st.write(map._repr_html_(), unsafe_allow_html=True)
